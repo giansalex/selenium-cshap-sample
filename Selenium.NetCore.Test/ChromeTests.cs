da50705a16115a6a95a950c449b268c84fb05b98
Giancarlos Salas <giansalex@gmail.com>
2018-03-28 12:30:21 -0500

    complete chomre test in .net core

--- Selenium.NetCore.Test/ChromeTests.cs
+++ Selenium.NetCore.Test/ChromeTests.cs
@@ -1,14 +1,22 @@
-using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using Xunit;
 
 namespace Selenium.NetCore.Test
 {
     public class ChromeTests
     {
+        private static IWebDriver _chrome;
+
+        public ChromeTests()
+        {
+            _chrome = new ChromeDriver(@"D:\Programs\selenium");
+        }
+
         [Fact]
         public void GotToIndex()
         {
-
+            _chrome.Navigate().GoToUrl("https://google.com.pe");
         }
     }
 }
