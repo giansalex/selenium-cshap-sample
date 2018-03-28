2cdd14e5e020b7029d5474ab78b1122c2a16d933
Giancarlos Salas <giansalex@gmail.com>
2018-03-28 12:22:12 -0500

    add selenium .net core 2 project

---
+++ Selenium.NetCore.Test/ChromeTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Selenium.NetCore.Test
+{
+    public class ChromeTests
+    {
+        [Fact]
+        public void GotToIndex()
+        {
+
+        }
+    }
+}
