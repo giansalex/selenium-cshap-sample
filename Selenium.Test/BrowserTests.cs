1293f1132e180521900e36fafbb436ac60269523
Giancarlos Salas <giansalex@gmail.com>
2019-05-27 10:57:04 -0500

    add firefox driver

--- Selenium.Test/BrowserTests.cs
+++ Selenium.Test/BrowserTests.cs
@@ -1,7 +1,10 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+using System.Reflection;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
 
 namespace Selenium.Test
 {
@@ -10,19 +13,33 @@ namespace Selenium.Test
     {
         private static IWebDriver _chrome;
         private static IWebDriver _edge;
+        private static IWebDriver _firefox;
 
         [SetUp]
-        public static void SetUp()
+        public void SetUp()
         {
-            _chrome = new ChromeDriver(@"D:\Programs\selenium");
-            _edge = new EdgeDriver(@"D:\Programs\selenium");
+            var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var pathDrivers = directory + "/../../../drivers";
+
+            _chrome = new ChromeDriver(pathDrivers);
+            _edge = new EdgeDriver(pathDrivers);
+            _firefox = new FirefoxDriver(pathDrivers);
         }
 
         [Test]
         public void GoToIndex()
         {
-            _chrome.Navigate().GoToUrl("https://google.com.pe");
-            _edge.Navigate().GoToUrl("https://www.microsoft.com");
+            _chrome.Navigate().GoToUrl("https://google.com.pe/");
+            _edge.Navigate().GoToUrl("https://www.microsoft.com/");
+            _firefox.Navigate().GoToUrl("http://www.mozilla.org/");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            _edge?.Dispose();
+            _chrome?.Dispose();
+            _firefox?.Dispose();
         }
     }
 }
